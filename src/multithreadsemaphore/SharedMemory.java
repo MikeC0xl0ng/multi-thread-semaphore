70d4bc7496c0862852960a650ded87d05b86e291
MikeC0xl0ng <bodliev.mykola@gmail.com>
2021-11-21 16:42:38 +0100

    [dev] Shared memory semaphore configuration

--- src/multithreadsemaphore/SharedMemory.java
+++ src/multithreadsemaphore/SharedMemory.java
@@ -1,11 +1,18 @@
 package multithreadsemaphore;
 
+import java.util.concurrent.Semaphore;
+
 class SharedMemory {
     
     int value;
+    private Semaphore semaphore;
 
     public SharedMemory() {
         this.value = -1;
+        this.semaphore = new Semaphore(1);
     }
     
+    boolean tryAccess(){
+        return semaphore.tryAcquire();
+    }
 }
