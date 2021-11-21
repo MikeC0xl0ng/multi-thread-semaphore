ab681f07702d85d70660601bdffffb7c97bca646
MikeC0xl0ng <bodliev.mykola@gmail.com>
2021-11-21 16:59:37 +0100

    [dev] Producer class thread configuration

--- src/multithreadsemaphore/Producer.java
+++ src/multithreadsemaphore/Producer.java
@@ -1,6 +1,6 @@
 package multithreadsemaphore;
 
-class Producer {
+class Producer extends Thread{
     SharedMemory [] sharedResources;
     
     
@@ -8,5 +8,30 @@ class Producer {
         this.sharedResources = sharedResources;
     }
     
+    @Override
+    public void run(){
+        while(true){
+            long threadId = Thread.currentThread().getId();
+            System.out.println("Thread " + threadId + ": starting...");
+            for(int i=0; i<sharedResources.length; i++){
+                if(sharedResources[i].tryAccess()){
+                    System.out.println("Thread " + threadId + ": inside of resource #" + i);
+                    System.out.println("Thread " + threadId + ": working with the resource...");
+                    sharedResources[i].value += 1;
+                    try{
+                        sleep(2000);
+                    }catch(Exception e){}
+                    System.out.println("Thread " + threadId + ": finished my work with the resource!");
+                    sharedResources[i].release();
+                    System.out.println("Thread " + threadId + ": released the resource!");
+                    i=-1;
+                }
+                if(i == (sharedResources.length-1)){
+                    i=0;
+                }
+            }
+            
+        }
+    }
     
 }
