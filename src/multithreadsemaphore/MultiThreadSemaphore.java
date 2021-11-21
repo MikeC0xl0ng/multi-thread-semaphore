c86b7746413a8d067b0a7b0ccd1a0deec6616699
MikeC0xl0ng <bodliev.mykola@gmail.com>
2021-11-21 16:58:52 +0100

    [fixes] Main

--- src/multithreadsemaphore/MultiThreadSemaphore.java
+++ src/multithreadsemaphore/MultiThreadSemaphore.java
@@ -7,16 +7,19 @@ package multithreadsemaphore;
 public class MultiThreadSemaphore {
     
     static final int NUMBER_SHARED_RESOURCES = 10;
-    static final int NUMBER_Producers = 5;
+    static final int NUMBER_PRODUCERS = 5;
 
     public static void main(String[] args) {
-        SharedMemory [] sharedMemories = new SharedMemory[NUMBER_SHARED_RESOURCES];
-        for(int i=0; i<sharedMemories.length; i++){
-            sharedMemories[i] = new SharedMemory();
+        SharedMemory [] sharedResources = new SharedMemory[NUMBER_SHARED_RESOURCES];
+        for(int i=0; i<sharedResources.length; i++){
+            sharedResources[i] = new SharedMemory();
         }
-        Producer [] producers = new Producer[NUMBER_Producers];
+        Producer [] producers = new Producer[NUMBER_PRODUCERS];
         for(int i=0; i<producers.length; i++){
-            producers[i] = new Producer();
+            producers[i] = new Producer(sharedResources);
+        }
+        for(int i=0; i<producers.length; i++){
+            producers[i].start();
         }
     }
     
